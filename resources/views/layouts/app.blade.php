1e156c8091c1d959d22bfc969ecaaffc233b931f
takeumi.ishihara <takeumiishihara@gmail.com>
2021-07-19 10:56:44 +0000

    CRUD tasks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,22 +8,10 @@
     </head>
 
     <body>
-        <header class="mb-4">
-            <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-                {{-- トップページへのリンク --}}
-                <a class="navbar-brand" href="/">TaskList</a>
+        
+        {{-- ナビゲーションバー --}}
+        @include('commons.navbar')
 
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="nav-bar">
-                    <ul class="navbar-nav mr-auto"></ul>
-                    <ul class="navbar-nav">
-                    </ul>
-                </div>
-            </nav>
-        </header>
 
         <div class="container">
             @yield('content')
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,4 +4,26 @@
 
 <!-- ここにページ毎のコンテンツを書く -->
 
+<h1>タスク一覧</h1>
+
+    @if (count($Tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タスク</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($Tasks as $task)
+                <tr>
+                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+    
+    {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
 @endsection
